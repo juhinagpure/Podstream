09fbaf7eb84203e762bb7a2ec3a96f8ef502b137
JUHI NAGPURE <juhinagpure143@gmail.com>
2024-04-01 21:35:37 +0530

    added images and styled component

--- src/components/PodcastCard.jsx
+++ src/components/PodcastCard.jsx
@@ -4,23 +4,23 @@ import Avatar from "@mui/material/Avatar";
 import { IconButton } from "@mui/material";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 
-const PlayIcon = styled.div`
-  padding: 10px;
-  border-radius: 50%;
-  z-index: 100;
-  display: flex;
-  align-items: center;
-  background: #9000ff !important;
-  color: white !important;
-  backdrop-filter: blur(4px);
-  -webkit-backdrop-filter: blur(4px);
-  position: absolute !important;
-  top: 45%;
-  right: 10%;
-  display: none;
-  transition: all 0.4s ease-in-out;
-  box-shadow: 0 0 16px 4px #9000ff50 !important;
-`;
+// const PlayIcon = styled.div`
+//   padding: 10px;
+//   border-radius: 50%;
+//   z-index: 100;
+//   display: flex;
+//   align-items: center;
+//   background: #9000ff !important;
+//   color: white !important;
+//   backdrop-filter: blur(4px);
+//   -webkit-backdrop-filter: blur(4px);
+//   position: absolute !important;
+//   top: 45%;
+//   right: 10%;
+//   display: none;
+//   transition: all 0.4s ease-in-out;
+//   box-shadow: 0 0 16px 4px #9000ff50 !important;
+// `;
 const Card = styled.div`
   position: relative;
   text-decoration: none;
@@ -41,9 +41,6 @@ const Card = styled.div`
     box-shadow: 0 0 18px 0 rgba(0, 0, 0, 0.3);
     filter: brightness(1.3);
   }
-  &:hover ${PlayIcon} {
-    display: flex;
-  }
 `;
 const Top = styled.div`
   display: flex;
@@ -53,12 +50,66 @@ const Top = styled.div`
   position: relative;
 `;
 
-const Favorite = styled(IconButton)``;
-const CardImage = styled.img``;
-const CardInformation = styled.div``;
-const MainInfo = styled.div``;
-const Title = styled.div``;
-const Decryption = styled.div``;
+const Favorite = styled(IconButton)`
+  color: white;
+  top: 8px;
+  right: 6px;
+  padding: 6px !important;
+  border-radius: 50%;
+  z-index: 100;
+  display: flex;
+  align-items: center;
+  background: ${({ theme }) => theme.text_secondary + 95} !important;
+  color: white !important;
+  position: absolute !important;
+  backdrop-filter: blur(4px);
+  box-shadow: 0 0 16px 6px #222423 !important;
+`;
+
+const CardImage = styled.img`
+object-fit: cover;
+  width: 220px;
+  height: 140px;
+  border-radius: 6px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.3);
+  &:hover{
+    box-shadow: 0 4px 30px rgba(0, 0, 0, 0.4);
+`;
+const CardInformation = styled.div`
+  display: flex;
+  align-items: flex-end;
+  font-weight: 450;
+  padding: 14px 0px 0px 0px;
+  width: 100%;
+`;
+const MainInfo = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: flex-start;
+  gap: 4px;
+`;
+const Title = styled.div`
+  overflow: hidden;
+  display: -webkit-box;
+  max-width: 100%;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  color: ${({ theme }) => theme.text_primary};
+`;
+const Description = styled.div`
+  overflow: hidden;
+  display: -webkit-box;
+  max-width: 100%;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  color: ${({ theme }) => theme.text_secondary};
+  font-size: 12px;
+`;
 const CreatersInfo = styled.div``;
 const Creator = styled.div``;
 const CreaterName = styled.div``;
@@ -71,19 +122,19 @@ const PodcastCard = () => {
         <Favorite>
           <FavoriteIcon />
         </Favorite>
-        <CardImage src="https://th.bing.com/th/id/OIP.6GSP_4rcbcVRkU7EtuKx7wHaFb?rs=1&pid=ImgDetMain" />
+        <CardImage src="" />
       </Top>
       <CardInformation>
         <MainInfo>
           <Title>The Tim Ferriss Show</Title>
-          <Decryption>
+          <Description>
             {" "}
             "The Tim Ferriss Show" is a popular podcast hosted by Tim Ferriss,
             an entrepreneur, author, and public speaker known for his books such
             as "The 4-Hour Workweek." In the podcast, Ferriss deconstructs the
             habits, routines, and strategies of world-class performers from
             diverse areas including investing, sports, business, art, and more.
-          </Decryption>
+          </Description>
           <CreatersInfo>
             <Creator>
               <Avatar>J</Avatar>
