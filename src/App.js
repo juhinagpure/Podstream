810e5de97d92ad1a32e805c22e3a8cfe5658c716
JUHI NAGPURE <juhinagpure143@gmail.com>
2024-04-04 12:12:37 +0530

    made a podstream project responsive

--- src/App.js
+++ src/App.js
@@ -1,51 +1,123 @@
-import { useState } from "react";
-import styled, { ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme } from "./utils/Themes";
-import Sidebar from "./components/Sidebar";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import Dashboard from "./pages/Dashboard.jsx";
-import Search from "./pages/Search.jsx";
-import Favourites from "./pages/Favourites.jsx";
-import Profile from "./pages/Profile.jsx";
+import { ThemeProvider } from "styled-components";
+import { useState, useEffect } from "react";
+import { darkTheme, lightTheme } from "./utils/Themes.js";
+import Signup from "../src/components/Signup.jsx";
+import Signin from "../src/components/Signin.jsx";
+import OTP from "../src/components/OTP.jsx";
+import NavBar from "../src/components/NavBar.jsx";
+import Menu from "../src/components/Menu.jsx";
+import Dashboard from "../src/pages/Dashboard.jsx";
+import ToastMessage from "./components/ToastMessage.jsx";
+import Search from "../src/pages/Search.jsx";
+import Favourites from "../src/pages/Favourites.jsx";
+import Profile from "../src/pages/Profile.jsx";
+import Upload from "../src/components/Upload.jsx";
+import DisplayPodcasts from "../src/pages/DisplayPodcasts.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import styled from "styled-components";
+import AudioPlayer from "./components/AudioPlayer.jsx";
+import VideoPlayer from "./components/VideoPlayer.jsx";
 import PodcastDetails from "./pages/PodcastDetails.jsx";
-import DisplayPodcast from "./pages/DisplayPodcast.jsx";
+import { closeSignin } from "./redux/setSigninSlice.jsx";
 
-const Container = styled.div`
-  display: flex;
-  background: ${({ theme }) => theme.bgLight};
-  width: 100%;
-  height: 100vh;
-  overflow-x: hidden;
-  overflow-y: hidden;
-`;
 const Frame = styled.div`
   display: flex;
   flex-direction: column;
   flex: 3;
 `;
 
+const Podstream = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100vh;
+  background: ${({ theme }) => theme.bgLight};
+  overflow-y: hidden;
+  overflow-x: hidden;
+`;
+
 function App() {
-  //hooks
   const [darkMode, setDarkMode] = useState(true);
+  const { open, message, severity } = useSelector((state) => state.snackbar);
+  const { openplayer, type, episode, podid, currenttime, index } = useSelector(
+    (state) => state.audioplayer
+  );
+  const { opensi } = useSelector((state) => state.signin);
+  const [SignUpOpen, setSignUpOpen] = useState(false);
+  const [SignInOpen, setSignInOpen] = useState(false);
   const [menuOpen, setMenuOpen] = useState(true);
+  const [uploadOpen, setUploadOpen] = useState(false);
+
+  const { currentUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+  //set the menuOpen state to false if the screen size is less than 768px
+  useEffect(() => {
+    const resize = () => {
+      if (window.innerWidth < 1110) {
+        setMenuOpen(false);
+      } else {
+        setMenuOpen(true);
+      }
+    };
+    resize();
+    window.addEventListener("resize", resize);
+    return () => window.removeEventListener("resize", resize);
+  }, []);
+
+  useEffect(() => {
+    dispatch(closeSignin());
+  }, []);
 
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <BrowserRouter>
-        <Container>
+        {opensi && (
+          <Signin setSignInOpen={setSignInOpen} setSignUpOpen={setSignUpOpen} />
+        )}
+        {SignUpOpen && (
+          <Signup setSignInOpen={setSignInOpen} setSignUpOpen={setSignUpOpen} />
+        )}
+        {uploadOpen && <Upload setUploadOpen={setUploadOpen} />}
+        {openplayer && type === "video" && (
+          <VideoPlayer
+            episode={episode}
+            podid={podid}
+            currenttime={currenttime}
+            index={index}
+          />
+        )}
+        {openplayer && type === "audio" && (
+          <AudioPlayer
+            episode={episode}
+            podid={podid}
+            currenttime={currenttime}
+            index={index}
+          />
+        )}
+        <Podstream>
           {menuOpen && (
-            <Sidebar
-              menuOpen={menuOpen}
+            <Menu
               setMenuOpen={setMenuOpen}
-              setDarkMode={setDarkMode}
               darkMode={darkMode}
+              setDarkMode={setDarkMode}
+              setUploadOpen={setUploadOpen}
+              setSignInOpen={setSignInOpen}
             />
           )}
           <Frame>
-            <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+            <NavBar
+              menuOpen={menuOpen}
+              setMenuOpen={setMenuOpen}
+              setSignInOpen={setSignInOpen}
+              setSignUpOpen={setSignUpOpen}
+            />
             <Routes>
-              <Route path="/" exact element={<Dashboard />} />
+              <Route
+                path="/"
+                exact
+                element={<Dashboard setSignInOpen={setSignInOpen} />}
+              />
               <Route path="/search" exact element={<Search />} />
               <Route path="/favourites" exact element={<Favourites />} />
               <Route path="/profile" exact element={<Profile />} />
@@ -53,11 +125,15 @@ function App() {
               <Route
                 path="/showpodcasts/:type"
                 exact
-                element={<DisplayPodcast />}
+                element={<DisplayPodcasts />}
               />
             </Routes>
           </Frame>
-        </Container>
+
+          {open && (
+            <ToastMessage open={open} message={message} severity={severity} />
+          )}
+        </Podstream>
       </BrowserRouter>
     </ThemeProvider>
   );
